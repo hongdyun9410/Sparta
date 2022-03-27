c3a9f33c85dbb65116a3284b7784763c52276adb
장동현 <dkwntbvj@naver.com>
2022-03-27 17:08:12 +0900

    크롤링 데이터 db에 넣기

--- pyhtonprac/crolling.py
+++ pyhtonprac/crolling.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
@@ -22,8 +25,12 @@ for tr in trs:
         title = a_tag.text
         rank = tr.select_one('td:nth-child(1) > img ')['alt']
         point = tr.select_one('td.point').text
-        print(rank , title , point)
-
+        doc ={
+            'rank':rank,
+            'title':title,
+            'point':point
+        }
+        db.movies.insert_one(doc)
 
 
 
